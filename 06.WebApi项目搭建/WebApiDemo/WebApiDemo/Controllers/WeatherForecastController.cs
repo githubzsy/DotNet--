afeddb30f295b3cdfc31bcb419b3507fb467488f
周诗雅 <zhoushiya@126.com>
2021-12-20 17:30:54 +0800

    添加示例

---
+++ 06.WebApi项目搭建/WebApiDemo/WebApiDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string GetTest()
+        {
+            return "Test";
+        }
+
+        // 有/表示是从根开始，没有表示是承接Controller的Route
+        [Route("{a}")]
+        [HttpPost]
+        public string PostTest([Required]string a)
+        {
+            return a;
+        }
+
+        [HttpPost]
+        public AnimalDto PostDto(AnimalDto dto)
+        {
+            return dto;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ 06.WebApi项目搭建/WebApiDemo/WebApiDemo/Models/AnimalDto.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *┌──────────────────────────────────────────┐
+ *│  描   述: Animal
+ *│  作   者: zhoushiya
+ *│  版   本: 1.0
+ *│  创建时间: 2021/12/20 16:10:10
+ *│  说   明:
+ *└──────────────────────────────────────────┘
+ */
+namespace WebApiDemo.Models
+{
+    /// <summary>
+    /// 动物Dto
+    /// </summary>
+    public class AnimalDto
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
