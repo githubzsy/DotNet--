93794a528fce0d6b34fa42a849129edb736e1c5b
诗雅 周 <zhoushiya@126.com>
2022-03-16 09:52:22 +0800

    添加一个动态代理示例

---
+++ 17.IOC/NetCoreDemos/NetCoreDemos/InstanceClass.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+
+namespace NetCoreDemos
+{
+    public class InstanceClass
+    {
+        public int x = 1;
+
+        private static InstanceClass _instance;
+
+        private static readonly object _lock = new object();
+
+        private InstanceClass()
+        {
+
+        }
+
+        public static InstanceClass GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        var instance = new InstanceClass();
+                        // 添加内存屏障，上下放的（汇编）指令不能穿越，避免了_instance.x=0的发生
+                        Interlocked.MemoryBarrier();
+                        _instance = instance;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ 17.IOC/NetCoreDemos/NetCoreDemos/ThreadStaticDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NetCoreDemos
+{
+    public class ThreadStaticDemo
+    {
+        [ThreadStatic] public static int A;
+
+        [ThreadStatic] public static int B;
+
+        public static void Set(int a,int b)
+        {
+            A = a;
+            B = b;
+        }
+
+
+    }
+}
